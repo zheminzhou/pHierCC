614f0e1d84699afa7ccffe4cd6c2aea485b7f452
Zhemin Zhou <zheminzhou@users.noreply.github.com>
2021-01-05 18:38:20 +0000

    Update examples.bash

--- examples.bash
+++ examples.bash
@@ -1,3 +1,3 @@
-./HierCC -p examples/YERwgMLST.cgMLSTv1.profile0.gz -o examples/YERwgMLST.cgMLSTv1.HierCC0
-./HierCC -p examples/YERwgMLST.cgMLSTv1.profile.gz -a examples/YERwgMLST.cgMLSTv1.HierCC0.npz -o examples/YERwgMLST.cgMLSTv1.HierCC
+./pHierCC -p examples/YERwgMLST.cgMLSTv1.profile0.gz -o examples/YERwgMLST.cgMLSTv1.HierCC0
+./pHierCC -p examples/YERwgMLST.cgMLSTv1.profile.gz -a examples/YERwgMLST.cgMLSTv1.HierCC0.npz -o examples/YERwgMLST.cgMLSTv1.HierCC
 ./HCCeval -p examples/YERwgMLST.cgMLSTv1.profile.gz -c examples/YERwgMLST.cgMLSTv1.HierCC.HierCC.gz -o examples/YERwgMLST.cgMLSTv1.HierCC.eval
