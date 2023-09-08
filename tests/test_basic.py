f0e888797c52893eb3cc73acc8961a6ba59a45c9
Nabil-Fareed Alikhan <nabil@happykhan.com>
2023-09-08 14:48:15 +0100

    Fixes

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -22,11 +22,10 @@ def test_missing_error():
     runner = CliRunner()
     result = runner.invoke(phierCC, ['-p', 'tests/profiles_missing_test.txt', '-o', 'test_missing_file', '-n', '12'])
     # decompress output 
-  #  with gzip.open('test_missing_file.HierCC.gz', 'rb') as f_in:
- #       with open('test_missing_file.HierCC', 'wb') as f_out:
-   #         shutil.copyfileobj(f_in, f_out)
+    with gzip.open('test_missing_file.HierCC.gz', 'rb') as f_in:
+        with open('test_missing_file.HierCC', 'wb') as f_out:
+           shutil.copyfileobj(f_in, f_out)
     # compare output
-    ##assert filecmp.cmp('tests/output_missing_test.txt', 'test_missing_file.HierCC')
-  
+    assert filecmp.cmp('tests/output_missing_test.txt', 'test_missing_file.HierCC')
 
 # Path: test_basic.py
